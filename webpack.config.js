9baa16cb378689c7b47a3431cb5c766dde61864c
KienNguyen <kien.nguyen@jmango360.com>
2017-09-20 16:41:29 +0700

    add npm install --save-dev style-loader css-loader add 2 module này ở file webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.js',
+  module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
