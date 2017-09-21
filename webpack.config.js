4583c6da8b7f7fa38fc395f98fb22667d434ea67
KienNguyen <kien.nguyen@jmango360.com>
2017-09-21 10:03:19 +0700

    add file loader, css loader, sass loader, node-sass, extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: ['./src/main.js'],
@@ -22,6 +23,15 @@ module.exports = {
       ],
 
     },
+	
+	plugins: [
+		new ExtractTextPlugin({ // define where to save the file
+		  filename: '[name].bundle.css',
+		  disalbe:false,
+		  allChunks: true,
+		}),
+	  ],
+	
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
